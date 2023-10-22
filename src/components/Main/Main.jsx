189af7c8767c6c940434b085e91d40748791cfb5
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-10-22 22:54:10 +0300

    верстка и JSX

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
-// import Techs from './Techs/Techs';
+import Techs from '../Techs/Techs';
 // import AboutMe from './AboutMe/AboutMe';
 // import Portfolio from './Portfolio/Portfolio';
 
@@ -11,9 +11,9 @@ export default function Main ({name, setLoggedIn}) {
          <main className="main">
             <Promo />
             <AboutProject />
-            {/* <Techs />
-            <AboutMe />
-            <Portfolio /> */}
+            <Techs />
+            {/* <AboutMe /> */}
+            {/* <Portfolio /> */}
          </main>
 
       </>
