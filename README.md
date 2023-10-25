414202a689878c06d5910ae73210b8932cb0ca4f
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-10-25 20:58:48 +0300

    верстка и JSX

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Front-end часть дипломной работы для Яндекс Практикума
 
-## Вёрстка по макету:
+## Вёрстка по макету
 
 [light-1](https://www.figma.com/file/6FMWkB94wE7KTkcCgUXtnC/Дипломный-проект?type=design&node-id=891-3857&mode=design&t=7GoPnCS3Emkx9udX-0)
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
-// import AboutMe from './AboutMe/AboutMe';
+import AboutMe from '../AboutMe/AboutMe';
 // import Portfolio from './Portfolio/Portfolio';
 
 
@@ -12,7 +12,7 @@ export default function Main ({name, setLoggedIn}) {
             <Promo />
             <AboutProject />
             <Techs />
-            {/* <AboutMe /> */}
+            <AboutMe />
             {/* <Portfolio /> */}
          </main>
 
